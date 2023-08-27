da3028c6509ededac863a9d4c487ed479712e4a3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-08-27 20:16:51 +0200

    chore: build library and bundle version

---
+++ build.js
@@ -0,0 +1,44 @@
+import { context } from "esbuild";
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+/**
+ * Library build of the module.
+ */
+const library = await context({
+  entryPoints: ["src/index.ts"],
+  outdir: "./dist",
+  bundle: false,
+  minify: isDevelopment === false,
+  plugins: [
+    {
+      name: "logger",
+      setup: ({ onEnd }) => onEnd(() => console.info("🎉 changes processed")),
+    },
+  ],
+});
+
+/**
+ * Bundled build of the module.
+ *
+ * To be consumed directly in the browser.
+ */
+const bundle = await context({
+  entryPoints: ["src/index.ts"],
+  outdir: "./dist/bundle",
+  bundle: true,
+  minify: isDevelopment === false,
+});
+
+if (isDevelopment) {
+  await library.watch({});
+  await bundle.watch({});
+  console.info("👀 watching for changes...");
+} else {
+  await library.rebuild();
+  await library.dispose();
+  console.info("✅ build library complete");
+  await bundle.rebuild();
+  await bundle.dispose();
+  console.info("✅ build bundle complete");
+}
